682e0f22865cd043bea765fb98be5a645bd50bb3
muntazircodes@gmail.com <muntazircodes@gmail.com>
2025-02-22 22:19:59 +0530

    written in prettier format

--- src/core/interfaces/ILibrary.ts
+++ src/core/interfaces/ILibrary.ts
@@ -1,26 +1,30 @@
 export interface ILibrary {
-   createLibrary(data: {
-      name: string;
-      address: string;
-      licenseNo: string;
-      libraryDocuments: string;
-      isVerified?: boolean;
-   }): Promise<any>;
-
-   getLibraryById(id: number): Promise<any | null>;
-
-   updateLibrary(
-      id: number,
-      updateData: Partial<{
+   createLibrary(
+      data: {
          name: string;
          address: string;
          licenseNo: string;
          libraryDocuments: string;
-         isVerified: boolean;
-      }>
-   ): Promise<any>;
+         isVerified?: boolean;
+      }
+): Promise<any>;
+
+
+updateLibrary(
+   id: number,
+   updateData: Partial<{
+      name: string;
+      address: string;
+      licenseNo: string;
+      libraryDocuments: string;
+      isVerified: boolean;
+   }>
+): Promise<any>;
+
+   getLibraryById(id: number): Promise<any | null>;
 
    deleteLibrary(id: number): Promise<boolean>;
 
    getAllLibraries(): Promise<any[]>;
+
 }
